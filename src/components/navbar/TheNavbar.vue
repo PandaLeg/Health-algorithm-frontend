3650a8ffe8afe57c1df67f23391a76eef21a84d3
PandaLeg <kurtlansfer@gmail.com>
2023-04-11 21:25:48 +0300

    built and styled navbar

---
+++ src/components/navbar/TheNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="header">
+        <div class="header__content">
+            <nav class="header__menu menu">
+                <div class="menu__icon icon-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="menu__body">
+                    <ul class="menu__list">
+                        <li><a href="#" class="menu__link">Main</a></li>
+                        <li><a href="#" class="menu__link">Doctors</a></li>
+                        <li><a href="#" class="menu__link">Clinics</a></li>
+                        <li><a href="#" class="menu__link">Log In</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "TheNavbar"
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/scss/variables.scss";
+@import "src/assets/scss/header.scss";
+</style>
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="wrapper">
+        <TheNavbar />
         <main class="page">
             <router-view></router-view>
         </main>
@@ -7,11 +8,16 @@
 </template>
 
 <script>
+import TheNavbar from "../components/navbar/TheNavbar.vue";
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        TheNavbar
+    }
 }
 </script>
 
-<style scoped lang="scss">
-@import "/src/assets/scss/style";
+<style lang="scss">
+@import "/src/assets/scss/style.scss";
 </style>
